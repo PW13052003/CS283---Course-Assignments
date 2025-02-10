e5ca371b629a027243a632b3babc87876dcf068f
pw425 <pw425@tux5.cci.drexel.edu>
2025-02-10 17:33:32 -0500

    Updated dshlib.c

--- 3-ShellP1/dshlib.c
+++ 3-ShellP1/dshlib.c
@@ -34,8 +34,64 @@
  */
 int build_cmd_list(char *cmd_line, command_list_t *clist)
 {
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+    // Trim trailing spaces
+    while (*cmd_line == SPACE_CHAR) {
+	    cmd_line++;
+    }
+    
+    if (*cmd_line == '\0') {
+	    return WARN_NO_CMDS;
+    }
+
+    memset (clist, 0, sizeof (command_list_t));
+    char *cmd_token;
+    int cmd_count = 0;
+
+    cmd_token = strtok (cmd_line, PIPE_STRING);
+    
+    while (cmd_token != NULL) {
+
+	   // Trim leading spaces in each command 
+	   while (*cmd_token == SPACE_CHAR) {
+		  cmd_token++;
+	   }
+
+	   // Check if there are too many commands
+	   if (cmd_count >= CMD_MAX) {
+		  return ERR_TOO_MANY_COMMANDS;
+	   } 
+
+	   // Extract command name
+	   char *arg_token = strtok (cmd_token, " ");
+
+	   if (arg_token == NULL) {
+		   cmd_token = strtok(NULL, PIPE_STRING);  // Move to next command
+		   continue;
+	   }
+
+	   strncpy (clist->commands[cmd_count].exe, arg_token, EXE_MAX - 1);
+	   
+	   // Extract arguments
+	   char args_buffer[ARG_MAX] = "";
+	   arg_token = strtok(NULL, "");
+
+	   if (arg_token != NULL) {
+		   strncpy (args_buffer, arg_token, ARG_MAX - 1);
+	   }
+	   
+	   // Store parsed values in clist
+	   strncpy (clist->commands[cmd_count].args, args_buffer, ARG_MAX - 1);
+	   
+	   cmd_count++;
+
+	   // Get next command
+	   cmd_token = strtok (NULL, PIPE_STRING);
+
+    }
+
+    // Store the number of commands parsed
+    clist->num = cmd_count;
+    return OK;
 }
 
 
