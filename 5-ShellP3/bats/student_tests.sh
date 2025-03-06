5a450f9f7eb6ff58951f6ee9cefb7e959e03bdb0
pw425 <pw425@tux4.cci.drexel.edu>
2025-03-05 23:30:22 -0500

    Updated all

--- 5-ShellP3/bats/student_tests.sh
+++ 5-ShellP3/bats/student_tests.sh
@@ -4,6 +4,26 @@
 # 
 # Create your unit tests suit in this file
 
+@test "No piping with ls" {
+    run "./dsh" <<EOF
+ls
+EOF
+    # Strip all whitespace from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    
+    # Get the expected output
+    expected_output="batsdshdsh_cli.cdshlib.cdshlib.hmakefilequestions.mddsh3>dsh3>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print if the test fails
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check the exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
+
 @test "Two piped commands" {	
     run "./dsh" <<EOF
 ls | grep dshlib
@@ -88,7 +108,7 @@ EOF
     [ "$stripped_output" = "$expected_output" ]
 }
 
-@test "Long pipeline" {
+@test "Long pipeline - 1" {
     run "./dsh" <<EOF
 ls | grep dsh | sort | uniq | wc -l
 EOF
@@ -98,6 +118,27 @@ EOF
 
     # Get the expected output
     expected_output="4dsh3>dsh3>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print if the test fails
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
+
+@test "Long pipeline - 2" {
+    run "./dsh" <<EOF
+ls | sort | uniq | grep ".c" | awk '{print \$1}' | sed 's/.c//g'
+EOF
+
+    # Strip all whitespace from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Get the expected output
+    expected_output="grep::Nosuchfileordirectorydsh3>dsh3>cmdloopreturned0"
 
     # These echo commands will help with debugging and will only print if the test fails
     echo "Captured stdout:"
@@ -151,7 +192,6 @@ EOF
     [ "$stripped_output" = "$expected_output" ]
 }
 
-
 @test "Pipeline with awk" {
     run "./dsh" <<EOF
 echo "1 2 3" | awk '{print \$2}'
@@ -194,5 +234,87 @@ EOF
     [ "$stripped_output" = "$expected_output" ]
 }
 
+@test "Pipeline with exec" {
+    run "./dsh" <<EOF
+exec echo "hello" | wc -l
+EOF
+  
+    # Strip all whitespace from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
 
+    # Get the expected output
+    expected_output="execvpfailed:Nosuchfileordirectory0dsh3>dsh3>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print if the test fails
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
+
+@test "Pipeline with nonexistent command" {
+    run "./dsh" <<EOF
+ls | nonexistent_command | wc -l
+EOF
+
+    # Strip all whitespace from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Get the expected output
+    expected_output="execvpfailed:Nosuchfileordirectory0dsh3>dsh3>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print if the test fails
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
+
+@test "Pipeline with SIGINT Handling" {
+    run "./dsh" <<EOF
+yes | head -n 5
+EOF
+
+    # Strip all whitespace from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Get the expected output
+    expected_output="yyyyydsh3>dsh3>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print if the test fails
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
+
+@test "Pipeline with command that fails" {
+    run "./dsh" <<EOF
+ls | grep dsh | false | wc -l
+EOF
+   
+    # Strip all whitespace from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Get the expected output
+    expected_output="0dsh3>dsh3>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print if the test fails
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
 
